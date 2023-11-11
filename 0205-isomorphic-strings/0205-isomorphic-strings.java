98791dd1ab421d764aa1a9598b6695a7374726ef
Rajkumar Choudhury <91613575+Rajkumar562@users.noreply.github.com>
2023-11-11 11:35:32 +0530

    Time: 7 ms (86.57%), Space: 41.9 MB (14.31%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int a1[]=new int[200];
+        int a2[]=new int[200];
+
+        for(int i=0;i<s.length();i++)
+        {
+            if(a1[s.charAt(i)]!=a2[t.charAt(i)])
+            return false;
+
+            a1[s.charAt(i)]=i+1;
+            a2[t.charAt(i)]=i+1;
+        }
+        return true;
+    }
+}
